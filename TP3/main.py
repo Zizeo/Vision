da1af706ddd1342850702e10327d93c50f044978
Zizeo <elio.genson@gmail.com>
2024-12-18 17:30:21 +0100

    added TP3/4

---
+++ TP3/main.py
@@ -0,0 +1,107 @@
+from math import cos
+from matplotlib import image
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+
+def imagesc(img1):
+    img2 = (img1 - np.min(img1)) / (np.max(img1) - np.min(img1))
+    plt.imshow(img2, cmap="gray")
+    plt.show()
+
+
+# img = cv.imread("TP3/TP3I01.jpg")
+# if img is None:
+#     raise Exception("Image introuvable")
+
+# img = cv.cvtColor(img, cv.COLOR_BGR2GRAY).astype(float)
+
+# kernel1 = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]]) / 3
+# kernel2 = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]]) / 3
+
+# filtered1 = cv.filter2D(img, -1, kernel1)
+# filtered2 = cv.filter2D(img, -1, kernel2)
+# filtered = abs(filtered1) + abs(filtered2)
+# max_ = np.max(filtered)
+# binarized = np.where(filtered > 10, 255, 0)
+
+# plt.subplot(121), plt.imshow(img, cmap="gray")
+# plt.title("img originale"), plt.xticks([]), plt.yticks([])
+# plt.subplot(122), plt.imshow(binarized, cmap="gray")
+# plt.title("img binarisé"), plt.xticks([]), plt.yticks([])
+# plt.show()
+
+# shape_ = binarized.shape
+# H = np.zeros((shape_[0] + shape_[1], 360))
+# ro = None
+
+# for x in range(shape_[0]):
+#     for y in range(shape_[1]):
+#         if binarized[x, y] == 255:
+#             for t in range(0, 360, 1):
+#                 ro = int(x * np.cos(t * np.pi / 180) + y * np.sin(t * np.pi / 180))
+#                 if 0 <= ro < shape_[0] + shape_[1]:
+#                     H[ro, t] += 1
+
+
+# imagesc(H)
+
+# lst_x1, lst_y1, lst_x2, lst_y2 = [], [], [], []
+# k = 10
+# epsilum = 0.01
+# indices = np.argsort(H, axis=None)[-k:]
+# for index in indices:
+#     rho = index // 360
+#     theta = index % 360
+#     x1, y1, x2, y2 = None, None, None, None
+#     for x in range(shape_[0]):
+#         for y in range(shape_[1]):
+#             if (
+#                 binarized[x, y] == 255
+#                 and abs(
+#                     x * np.cos(theta * np.pi / 180)
+#                     + y * np.sin(theta * np.pi / 180)
+#                     - rho
+#                 )
+#                 < epsilum
+#             ):
+#                 if x1 is None or x < x1:
+#                     x1, y1 = x, y
+#                 if x2 is None or x > x2:
+#                     x2, y2 = x, y
+#     if x1 is not None and x2 is not None:
+#         print("Point", k, ":", rho, theta, "(", x1, ",", y1, ") (", x2, ",", y2, ")")
+#         lst_x1.append(x1)
+#         lst_y1.append(y1)
+#         lst_x2.append(x2)
+#         lst_y2.append(y2)
+#     k -= 1
+
+
+# for x1, y1, x2, y2 in zip(lst_x1, lst_y1, lst_x2, lst_y2):
+#     plt.plot([x1, x2], [y1, y2], color="red", linewidth=1)
+
+
+# plt.show()
+
+
+img = cv.imread("TP3/TP3I02.png")
+img = cv.cvtColor(img, cv.COLOR_BGR2GRAY).astype(float)
+print(img.shape)
+img_radon = np.zeros((468, 500))
+
+cosines = [np.cos(t / 100) for t in range(628)]
+sines = [np.sin(t / 100) for t in range(628)]
+for x in range(468):
+    for y in range(500):
+        img_radon[x, y] = sum(
+            [
+                img[int(x * cosines[t] + y * sines[t]), t]
+                for t in range(628)
+                if 0 <= int(x * cosines[t] + y * sines[t]) < 969
+            ]
+        )
+
+
+imagesc(img_radon)
---
+++ TP4/main.py
@@ -0,0 +1,124 @@
+from copy import deepcopy
+from matplotlib import image
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+
+def imagesc(img1):
+    img2 = (img1 - np.min(img1)) / (np.max(img1) - np.min(img1))
+    plt.imshow(img2, cmap="gray")
+    plt.show()
+
+
+cercle_img = cv.imread("TP4/cercle.png")
+carre_img = cv.imread("TP4/carre.png")
+triangle_img = cv.imread("TP4/triangle.png")
+cercle_img = cv.cvtColor(cercle_img, cv.COLOR_BGR2GRAY).astype(float)
+carre_img = cv.cvtColor(carre_img, cv.COLOR_BGR2GRAY).astype(float)
+triangle_img = cv.cvtColor(triangle_img, cv.COLOR_BGR2GRAY).astype(float)
+
+
+cercle_bin = np.where(cercle_img > 68, 0, 1)
+carre_bin = np.where(carre_img > 68, 0, 1)
+triangle_bin = np.where(triangle_img > 68, 0, 1)
+
+chanfrein_up = np.array([[4, 3, 4], [3, 0, np.inf], [np.inf, np.inf, np.inf]])
+chanfrein_low = np.array([[np.inf, np.inf, np.inf], [np.inf, 0, 3], [4, 3, 4]])
+
+cercle_pad = np.pad(cercle_bin, 1, mode="constant", constant_values=0)
+carre_pad = np.pad(carre_bin, 1, mode="constant", constant_values=0)
+triangle_pad = np.pad(triangle_bin, 1, mode="constant", constant_values=0)
+
+
+def distance_transform(img):
+    distance_img = np.full(
+        img.shape, np.inf, dtype=float
+    )  # Initialize with infinity as float
+    for i in range(1, img.shape[0] - 1):
+        for j in range(1, img.shape[1] - 1):
+            if img[i, j] == 0:
+                distance_img[i, j] = np.inf
+                print("inf")
+            if img[i, j] == 1:
+                distance_img[i, j] = 0
+                print(i, j)
+
+    # imagesc(distance_img)
+    # Upper pass
+    for i in range(1, img.shape[0] - 1):
+        for j in range(1, img.shape[1] - 1):
+            for k in range(3):
+                for l in range(3):
+                    # if k == 1 and l == 1:
+                    #     continue
+                    ni, nj = i + k - 1, j + l - 1
+                    if 0 <= ni < img.shape[0] and 0 <= nj < img.shape[1]:
+                        distance_img[i, j] = min(
+                            distance_img[i, j],
+                            distance_img[ni, nj] + chanfrein_up[k, l],
+                        )
+
+    # Lower pass
+    for i in range(img.shape[0] - 2, 0, -1):
+        for j in range(img.shape[1] - 2, 0, -1):
+            for k in range(3):
+                for l in range(3):
+                    # if k == 1 and l == 1:
+                    #     continue
+                    ni, nj = i + k - 1, j + l - 1
+                    if 0 <= ni < img.shape[0] and 0 <= nj < img.shape[1]:
+                        distance_img[i, j] = min(
+                            distance_img[i, j],
+                            distance_img[ni, nj] + chanfrein_low[k, l],
+                        )
+
+    return distance_img
+
+
+distance_carre = distance_transform(carre_bin)
+distance_triangle = distance_transform(triangle_bin)
+distance_cercle = distance_transform(cercle_bin)
+
+for i in range(distance_carre.shape[0]):
+    for j in range(distance_carre.shape[1]):
+        if distance_carre[i, j] == np.inf:
+            distance_carre[i, j] = 0
+        if distance_triangle[i, j] == np.inf:
+            distance_triangle[i, j] = 0
+        if distance_cercle[i, j] == np.inf:
+            distance_cercle[i, j] = 0
+
+# imagesc(distance_carre)
+# imagesc(distance_triangle)
+# imagesc(distance_cercle)
+
+cercle2 = cv.imread("TP4/cercle2.png")
+carre2 = cv.imread("TP4/carre2.png")
+triangle2 = cv.imread("TP4/triangle2.png")
+cercle2 = cv.cvtColor(cercle2, cv.COLOR_BGR2GRAY).astype(float)
+carre2 = cv.cvtColor(carre2, cv.COLOR_BGR2GRAY).astype(float)
+triangle2 = cv.cvtColor(triangle2, cv.COLOR_BGR2GRAY).astype(float)
+
+
+score_carre = {}
+score_triangle = {}
+score_cercle = {}
+
+score_carre["carre2"] = float(np.sum(distance_carre * carre2).astype(float))
+score_carre["triangle2"] = float(np.sum(distance_carre * triangle2).astype(float))
+score_carre["cercle2"] = float(np.sum(distance_carre * cercle2).astype(float))
+score_triangle["triangle2"] = float(np.sum(distance_triangle * triangle2).astype(float))
+score_triangle["cercle2"] = float(np.sum(distance_triangle * cercle2).astype(float))
+score_triangle["carre2"] = float(np.sum(distance_triangle * carre2).astype(float))
+score_cercle["carre2"] = float(np.sum(distance_cercle * carre2).astype(float))
+score_cercle["triangle2"] = float(np.sum(distance_cercle * triangle2).astype(float))
+score_cercle["cercle2"] = float(np.sum(distance_cercle * cercle2).astype(float))
+
+score_carre = dict(sorted(score_carre.items(), key=lambda x: x[1]))
+score_triangle = dict(sorted(score_triangle.items(), key=lambda x: x[1]))
+score_cercle = dict(sorted(score_cercle.items(), key=lambda x: x[1]))
+
+print("carre: ", score_carre)
+print("triangle: ", score_triangle)
+print("cercle: ", score_cercle)
